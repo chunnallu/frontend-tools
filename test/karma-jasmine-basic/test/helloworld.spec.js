75faffd485d774f6f2eaf15ab186b60fd524915f
luchunlong <luchunlong@outsidelook.cn>
2017-11-08 09:49:42 +0800

    增加了测试用例的个数

--- test/karma-jasmine-basic/test/helloworld.spec.js
+++ test/karma-jasmine-basic/test/helloworld.spec.js
@@ -10,8 +10,17 @@ describe(('helloworld controller'),function(){
 		ctrl = new HelloWorldController();
 	});
 
-	it('add function should add two number',function(){
+	it('add should be correct',function(){
 		expect(ctrl.add(1,2)).toBe(3);
+		expect(ctrl.add(-1,2)).toBe(1);
+		expect(ctrl.add(-1,-1)).toBe(-2);
+		expect(ctrl.add(0,0)).toBe(0);
+	})
+
+	it('mod should be mod correct',() =>{
+		expect(ctrl.mod(10,2)).toBe(0);
+		expect(ctrl.mod(0,2)).toBe(0);
+		expect(ctrl.mod(2,0)).toBeNaN();
 	})
 }
 )
